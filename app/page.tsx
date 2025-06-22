da58a911bbfc1bf82bcf70b7b4ec37dd518e536f
TheBlueBaron <ronniehughjames@protonmail.com>
2025-06-22 15:04:13 +0100

    feat(ui, auth): implemented auth database and github login functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,38 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function Home() {
+  const router = useRouter();
+  const { data: session } = authClient.useSession();
+
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+          toast.success("Signed out successfully.");
+        },
+      },
+    });
+  }
   return (
     <div>
       <h1 className="text-2xl font-bold text-red-500">Hello World</h1>
       <ThemeToggle />
+
+      {session ? (
+        <div>
+          <p>{session.user.name}</p>
+          <Button onClick={signOut}>Logout</Button>
+        </div>
+      ) : (
+        <Button>Login</Button>
+      )}
     </div>
   );
 }
